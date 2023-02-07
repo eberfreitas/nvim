be960f97bf7c5afdb10180d10893fc466b733cda
Éber Freitas Dias <eber.freitas@gmail.com>
2023-02-07 01:33:27 -0300

    Formatting and improvements

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,85 +1,83 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {},
-  cmd = "Mason"
-},
-{
-  "williamboman/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      "sumneko_lua",
-      "elmls",
-      "tsserver",
-      "html",
-      "cssls",
-    }
-  }
-},
-{
-  "neovim/nvim-lspconfig",
-  event = "BufReadPre",
-  dependencies = {
+  {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp"
+    opts = {},
+    cmd = "Mason"
   },
-  config = function()
-    local opts = { noremap = true, silent = true }
-
-    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
-
-    local on_attach = function(_, bufnr)
-      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-      local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-      vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-      vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-      vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, bufopts)
-      vim.keymap.set("n", "<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, bufopts)
-    end
-
-    local lsp_flags = {
-      debounce_text_changes = 150,
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "sumneko_lua",
+        "elmls",
+        "tsserver",
+        "html",
+        "cssls",
+      }
     }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = "BufReadPre",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp"
+    },
+    config = function()
+      local opts = { noremap = true, silent = true }
 
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
-    local lspconfig = require("lspconfig")
+      local on_attach = function(_, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({
-          on_attach = on_attach,
-          flags = lsp_flags,
-          capabilities = capabilities
-        })
-      end,
-      ["sumneko_lua"] = function()
-        lspconfig.sumneko_lua.setup({
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" }
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+        vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, bufopts)
+        vim.keymap.set("n", "<leader>wl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+      end
+
+      local lsp_flags = { debounce_text_changes = 150 }
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            on_attach = on_attach,
+            flags = lsp_flags,
+            capabilities = capabilities
+          })
+        end,
+        ["sumneko_lua"] = function()
+          lspconfig.sumneko_lua.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" }
+                }
               }
             }
-          }
-        })
-      end
-    })
-  end
+          })
+        end
+      })
+    end
+  }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "[F]ind [f]iles" },
     { "<leader>fg", "<cmd>Telescope find_files<cr>", desc = "[F]ind [g]it files" },
+    { "<leader>p", "<cmd>Telescope find_files<cr>", desc = "Find git files - alternative" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "[F]ind [b]uffers" },
     { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" }
   }
