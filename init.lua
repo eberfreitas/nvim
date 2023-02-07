dbc0c60f8935271a4f75059b575cb4d09547896a
Éber Freitas Dias <eber.freitas@gmail.com>
2023-02-06 23:38:24 -0300

    Clean up and key bindins

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme gruvbox]])
+      vim.cmd.colorscheme("gruvbox")
     end
   },
   {
@@ -129,29 +129,29 @@ require("lazy").setup({
     config = function()
       local opts = { noremap = true, silent = true }
 
-      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
-      vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, opts)
-      vim.keymap.set('n', ']g', vim.diagnostic.goto_next, opts)
-      vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
       local on_attach = function(_, bufnr)
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-        vim.keymap.set('n', '<leader>wl', function()
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+        vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, bufopts)
+        vim.keymap.set("n", "<leader>wl", function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, bufopts)
       end
@@ -185,12 +185,23 @@ require("lazy").setup({
   }
 })
 
-vim.cmd([[set hidden]])
-vim.cmd([[set number]])
-vim.cmd([[set relativenumber]])
-vim.cmd([[set list]])
-vim.cmd([[set listchars=tab:→\ ,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extends:»]])
-vim.cmd([[set ruler]])
-vim.cmd([[set nosmd]])
-vim.cmd([[set cursorline]])
-vim.cmd([[set cc=120]])
+vim.cmd.set("hidden")
+vim.cmd.set("number")
+vim.cmd.set("relativenumber")
+vim.cmd.set("list")
+vim.cmd.set("listchars=tab:→\\ ,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extends:»")
+vim.cmd.set("ruler")
+vim.cmd.set("nosmd")
+vim.cmd.set("cursorline")
+vim.cmd.set("cc=120")
+vim.cmd.set("t_Co=256")
+vim.cmd.set("bg=dark")
+vim.cmd.set("tabstop=4")
+vim.cmd.set("softtabstop=0")
+vim.cmd.set("shiftwidth=4")
+vim.cmd.set("expandtab")
+
+vim.keymap.set("n", "<C-k>", "<cmd>bp<cr>")
+vim.keymap.set("n", "<C-j>", "<cmd>bn<cr>")
+vim.keymap.set("n", "<C-x>", "<cmd>bd<cr>")
+vim.keymap.set("n", "<C-s>", "<cmd>w<cr>")
