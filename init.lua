c5ea958f4bc8b85445974e248d128dd9290619a0
Éber Freitas Dias <eber.freitas@gmail.com>
2023-02-07 00:11:11 -0300

    Autocomplete and stuff...

--- init.lua
+++ init.lua
@@ -122,9 +122,11 @@ require("lazy").setup({
   },
   {
     "neovim/nvim-lspconfig",
+    event = "BufReadPre",
     dependencies = {
       "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim"
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp"
     },
     config = function()
       local opts = { noremap = true, silent = true }
@@ -160,13 +162,16 @@ require("lazy").setup({
         debounce_text_changes = 150,
       }
 
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
 
       require("mason-lspconfig").setup_handlers({
         function(server_name)
           lspconfig[server_name].setup({
             on_attach = on_attach,
-            flags = lsp_flags
+            flags = lsp_flags,
+            capabilities = capabilities
           })
         end,
         ["sumneko_lua"] = function()
@@ -182,7 +187,74 @@ require("lazy").setup({
         end
       })
     end
-  }
+  },
+  -- Snipets & Autocomplete adapted from:
+  -- https://github.com/LazyVim/LazyVim/blob/1aa9e26e84726240d834da3599f61569c136141f/lua/lazyvim/plugins/coding.lua
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true, silent = true, mode = "i",
+      },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true })
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        experimental = {
+          ghost_text = {
+            hl_group = "LspCodeLens",
+          },
+        },
+      }
+    end
+  },
 })
 
 vim.cmd.set("hidden")
