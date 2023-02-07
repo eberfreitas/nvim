cb90514607e00fa02ccdafb5d8cb645b312e82e9
Éber Freitas Dias <eber.freitas@gmail.com>
2023-02-07 01:22:05 -0300

    Keeps only essential tree sitters

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,19 +3,15 @@ return {
   build = ":TSUpdate",
   opts = {
     ensure_installed = {
-      "c",
       "lua",
       "vim",
       "help",
       "elm",
-      "elixir",
       "javascript",
       "typescript",
       "html",
       "css",
-      "scss",
-      "ruby",
-      "rust"
+      "scss"
     },
   }
 }
